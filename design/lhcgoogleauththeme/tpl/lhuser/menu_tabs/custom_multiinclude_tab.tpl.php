c3d075c423ea648f80a00956f80c09a4ebec0408
Remigijus Kiminas <remdex@gmail.com>
2023-01-12 07:32:33 -0500

    Migrate BS5

--- design/lhcgoogleauththeme/tpl/lhuser/menu_tabs/custom_multiinclude_tab.tpl.php
+++ design/lhcgoogleauththeme/tpl/lhuser/menu_tabs/custom_multiinclude_tab.tpl.php
@@ -1,1 +1,1 @@
-<li role="presentation" class="nav-item"><a class="nav-link" href="#googleauth" aria-controls="googleauth" role="tab" data-toggle="tab"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/googleauth','Google Auth');?></a></li>
+<li role="presentation" class="nav-item"><a class="nav-link" href="#googleauth" aria-controls="googleauth" role="tab" data-bs-toggle="tab"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/googleauth','Google Auth');?></a></li>
--- design/lhcgoogleauththeme/tpl/lhuser/menu_tabs_content/custom_multiinclude_tab.tpl.php
+++ design/lhcgoogleauththeme/tpl/lhuser/menu_tabs_content/custom_multiinclude_tab.tpl.php
@@ -1,7 +1,7 @@
 <div role="tabpanel" class="tab-pane" id="googleauth">
     <?php if (erLhcoreClassUser::instance()->hasAccessTo('lhgoogleauth','use_admin')) : ?>
         <?php foreach (erLhcoreClassModelGoogleAuth::getList(array('filter' => array('user_id' => erLhcoreClassUser::instance()->getUserID()))) as $loggedAccount) : ?>
-        <a class="btn btn-sm btn-danger mb-1 mr-1" ng-non-bindable href="<?php echo  erLhcoreClassDesign::baseurl('googleauth/remove')?>/<?php echo $loggedAccount->id?>">Logout - <?php echo htmlspecialchars($loggedAccount->givenName . ' '. 	$loggedAccount->familyName . '[' . $loggedAccount->email . ']')?></a>
+        <a class="btn btn-sm btn-danger mb-1 me-1" ng-non-bindable href="<?php echo  erLhcoreClassDesign::baseurl('googleauth/remove')?>/<?php echo $loggedAccount->id?>">Logout - <?php echo htmlspecialchars($loggedAccount->givenName . ' '. 	$loggedAccount->familyName . '[' . $loggedAccount->email . ']')?></a>
         <?php endforeach; ?>
     <?php endif; ?>
     <br/>
